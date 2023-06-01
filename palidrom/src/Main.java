6ef3aa8e9f2bf4c58cac48245cd23a3e7c563460
R3v3rS <pepsi0192@gmail.com>
2023-06-01 10:32:45 +0200

    commit

--- palidrom/src/Main.java
+++ palidrom/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.println("is ok");
             char[] word2 = word1.toCharArray();
             for ( int i =0; i < word2.length/2;i++){
-                System.out.println("Porównuje char " + word2[i] +" oraz " + word2[word2.length-1-i]);
+                System.out.println("Porównuje znak " + word2[i] +" oraz " + word2[word2.length-1-i]);
                 if (word2[i] != word2[word2.length-i-1]){
                     System.out.println("Podane słowo nie jest palidromem");
                     polidrome = false;
